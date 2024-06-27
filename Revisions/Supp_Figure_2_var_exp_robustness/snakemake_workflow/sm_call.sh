d8cf75d5a296bb3dd2453d5e20ff014fbcb94c26
lauwill-nih <william.lau@nih.gov>
2024-06-27 12:21:21 -0400

    Update sm_call.sh
    changed script filename

--- Revisions/Supp_Figure_2_var_exp_robustness/snakemake_workflow/sm_call.sh
+++ Revisions/Supp_Figure_2_var_exp_robustness/snakemake_workflow/sm_call.sh
@@ -4,4 +4,4 @@ module load snakemake
 #module load R/3.6.0-goolf-1.7.20 
 source ~/.moduleload/R3.6.0_v2
 
-snakemake -j 500 --cluster-config cluster_config.json --cluster "qsub -terse -l quick,avx2,mem_free={cluster.mem_free},h_vmem={cluster.h_vmem} {cluster.parallel_opts}" --rerun-incomplete -s Snakefile.py
+snakemake -j 500 --cluster-config cluster_config.json --cluster "qsub -terse -l quick,avx2,mem_free={cluster.mem_free},h_vmem={cluster.h_vmem} {cluster.parallel_opts}" --rerun-incomplete -s Snakefile
